d90f56b937af7105208cd240f689fd596383e865
root <root@localhost.localdomain>
2017-01-15 12:05:11 -0500

    changes in submit

--- submit.py
+++ submit.py
@@ -18,13 +18,14 @@ HONOR_PLEDGE = "Honor Pledge:\n\n  \"I have neither given nor received aid on th
 
 def require_pledges():
   print(LATE_POLICY)
-  ans = raw_input("Please type 'yes' to agree and continue>")
+  ans = input("Please type 'yes' to agree and continue>")
+
   if ans != "yes":
     raise RuntimeError("Late policy not accepted.")
 
   print
   print(HONOR_PLEDGE)
-  ans = raw_input("Please type 'yes' to agree and continue>")
+  ans = input("Please type 'yes' to agree and continue>")
   if ans != "yes":
     raise RuntimeError("Honor pledge not accepted")
   print
@@ -67,6 +68,7 @@ def display_game(submission):
 
 def main():
   parser = argparse.ArgumentParser(description='Submits code to the Udacity site.')
+  #parser.add_argument('part', choices = ['assignment_1'])
   parser.add_argument('part', choices = ['assignment_1', 'play_isolation'])
   parser.add_argument('--provider', choices = ['gt', 'udacity'], default = 'gt')
   parser.add_argument('--environment', choices = ['local', 'development', 'staging', 'production'], default = 'production')
@@ -78,16 +80,18 @@ def main():
     require_pledges()
     quiz = 'assignment_1'
     filenames = ["player_submission.py"]
-
+    
+  if args.enable_face_off:
+    filenames.append("AI.txt")
     # Add AI.txt if face off is enabled
-    if parser.enable_face_off:
-      filenames.append("AI.txt")
+    #if parser.enable_face_off:
+    #  filenames.append("AI.txt")
 
-  else: # args.part == 'play_isolation'
-    quiz = 'play_isolation'
-    filenames = ["player_submission.py", "challenge_config.json"]
+  #else: # args.part == 'play_isolation'
+  #  quiz = 'play_isolation'
+  #  filenames = ["player_submission.py", "challenge_config.json"]
 
-  print "Submission processing...\n"
+  print ("Submission processing...\n")
   submission = Submission('cs6601', quiz,
                           filenames = filenames,
                           environment = args.environment,
