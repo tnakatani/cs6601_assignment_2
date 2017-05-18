bcc944d94dda417a56e7afa9cfade6a80435a001
Joshua Robusto <joshuarobusto@gmail.com>
2017-05-18 19:08:02 -0400

    Added uncertainty to __apply_move__ and refactored printing

--- isolation.py
+++ isolation.py
@@ -2,8 +2,8 @@ from copy import deepcopy
 from time import time, sleep
 import platform
 # import io
+import random
 import StringIO
-
 import sys
 import os
 
@@ -23,6 +23,7 @@ class Board:
     __inactive_players_queen2__ = None
 
     def __init__(self, player_1, player_2, width=7, height=7):
+        self.mistake_count = 0
         self.width = width
         self.height = height
 
@@ -70,11 +71,30 @@ class Board:
     def get_state(self):
         return deepcopy(self.__board_state__)
 
-    def __apply_move__(self, move):
+    def __apply_move__(self, move, uncertainty=True):
+        cur_row, cur_col = self.__last_queen_move__[self.__active_queen__]
         row, col = move
+        if uncertainty:
+            distance = max(abs(cur_row - row), abs(cur_col - col))
+            if distance < 2:
+                mistake_prob = 0
+            elif distance >= 5:
+                mistake_prob = 0.9
+            else:
+                mistake_prob = (0.4, 0.6, 0.8)[distance - 2]
+
+            if random.random() < mistake_prob:
+                directions = [(-1, -1), (-1, 0), (-1, 1),
+                              (0, -1), (0, 1),
+                              (1, -1), (1, 0), (1, 1)]
+                mistakes = [(row + dr, col + dc) for dr, dc in directions
+                            if self.move_is_legal(row + dr, col + dc)]
+                if mistakes:
+                    self.mistake_count += 1
+                    move = random.choice(mistakes)
+                    row, col = move
         self.__last_queen_move__[self.__active_queen__] = move
         self.__board_state__[row][col] = self.__queen_symbols__[self.__active_queen__]
-
         # swap the players
 
         tmp = self.__active_player__
@@ -92,6 +112,7 @@ class Board:
         self.__inactive_players_queen2__ = tmp
 
         self.move_count = self.move_count + 1
+        return move
 
     def __apply_move_write__(self, move, __active_queen__):
         row, col = move
@@ -135,7 +156,7 @@ class Board:
     def forecast_move(self, move, queen):
         new_board = self.copy()
         new_board.set_active_queen(queen)
-        new_board.__apply_move__(move)
+        new_board.__apply_move__(move, uncertainty=False)
         return new_board
 
     def get_active_player(self):
@@ -165,15 +186,15 @@ class Board:
         }
         where (move_x, move_y) is the intended target, (pos_x, pos_y) is a space that move might result in, and
         likelihood is the chance that result happens.
-        
+
         An example of iterating through this nested dictionary is included below:
-        
+
         move_dict = self.get_opponent_moves()
         for move in move_dict:
             move_x, move_y = move
             for (pos_x, pos_y), likelihood in move_dict[move]:
                 # do something
-                
+
         All 4 queens must be placed on the board in the first 4 moves
         """
 
@@ -209,15 +230,12 @@ class Board:
         }
         where (move_x, move_y) is the intended target, (pos_x, pos_y) is a space that move might result in, and
         likelihood is the chance that result happens.
-
         An example of iterating through this nested dictionary is included below:
-
         move_dict = self.get_legal_moves()
         for move in move_dict:
             move_x, move_y = move
             for (pos_x, pos_y), likelihood in move_dict[move]:
                 # do something
-
         All 4 queens must be placed on the board in the first 4 moves
         """
         move_by_q1 = self.__last_queen_move__[self.get_queen_name(self.__active_players_queen1__)]
@@ -273,16 +291,16 @@ class Board:
         move_possibilities = {}
         for r, c in valid_moves:
             distance = max(abs(r - start_r), abs(c - start_c))
-            if distance <= 2:
+            if distance < 2:
                 move_possibilities[(r, c)] = ((r, c), 1.0)
             else:
                 mistakes = [(r + dr, c + dc) for dr, dc in directions
                             if self.move_is_legal(r + dr, c + dc)]
                 n_choices = len(mistakes)
                 if distance >= 5:
-                    mistake_prob = 0.33
+                    mistake_prob = 0.9
                 else:
-                    mistake_prob = (0.11, 0.22)[distance - 3]
+                    mistake_prob = (0.4, 0.6, 0.8)[distance - 2]
                 outcomes = [((r2, c2), mistake_prob / n_choices) for r2, c2 in mistakes]
                 outcomes.append(((r, c), 1 - mistake_prob))
                 move_possibilities[(r, c)] = outcomes[:]
@@ -333,36 +351,31 @@ class Board:
 
         return out
 
-    def play_isolation(self, time_limit=5000):
+    def play_isolation(self, time_limit=5000, print_moves=False):
         # changed the time_limit
-
+        self.output_history = []
         move_history = []
         queen_history = []
         mi = 1
 
         if platform.system() == 'Windows':
-            def curr_time_millis(): return int(round(time() * 1000))
+            def curr_time_millis():
+                return int(round(time() * 1000))
         else:
-            def curr_time_millis(): return 1000 * resource.getrusage(resource.RUSAGE_SELF).ru_utime
-
+            def curr_time_millis():
+                return 1000 * resource.getrusage(resource.RUSAGE_SELF).ru_utime
         while True:
             game_copy = self.copy()
             move_start = curr_time_millis()
             time_left = lambda: time_limit - (curr_time_millis() - move_start)
             curr_move = Board.NOT_MOVED
-            try:
-                legal_player_moves = self.get_legal_moves()
-                curr_move, queen = self.__active_player__.move(game_copy, legal_player_moves,
-                                                               time_left)  # queen added in return
-                if queen == None:
-                    return self.__inactive_player__, move_history, queen_history, "illegal move"
-                self.set_active_queen(queen)
 
-            except AttributeError as e:
-                raise e
-            except Exception as e:
-                print(e)
-                pass
+            legal_player_moves = self.get_legal_moves()
+            curr_move, queen = self.__active_player__.move(game_copy, legal_player_moves,
+                                                           time_left)  # queen added in return
+            if queen == None:
+                return self.__inactive_player__, move_history, queen_history, "illegal move"
+            self.set_active_queen(queen)
 
             if curr_move is None:
                 curr_move = Board.NOT_MOVED
@@ -375,47 +388,79 @@ class Board:
                 queen_history[-1].append(self.__active_queen__)
 
             if time_limit and time_left() <= 0:
+                if print_moves:
+                    print 'Winner: ' + str(self.__inactive_player__)
                 return self.__inactive_player__, move_history, queen_history, "timeout"
 
             legal_moves_of_queen1 = self.get_legal_moves_of_queen1()
             legal_moves_of_queen2 = self.get_legal_moves_of_queen2()
 
             if self.__active_players_queen1__ == queen and curr_move not in legal_moves_of_queen1:
+                if print_moves:
+                    print 'Winner: ' + str(self.__inactive_player__)
                 return self.__inactive_player__, move_history, queen_history, "illegal move"
 
             if self.__active_players_queen2__ == queen and curr_move not in legal_moves_of_queen2:
+                if print_moves:
+                    print 'Winner: ' + str(self.__inactive_player__)
                 return self.__inactive_player__, move_history, queen_history, "illegal move"
 
             if curr_move not in legal_moves_of_queen1 and curr_move not in legal_moves_of_queen2:
+                if print_moves:
+                    print 'Winner: ' + str(self.__inactive_player__)
                 return self.__inactive_player__, move_history, queen_history, "illegal move"
 
-            self.__apply_move__(curr_move)
+            last_attempt = curr_move
+            last_move = self.__apply_move__(curr_move)
+            if self.__active_player__ == self.__player_1__:
+                move_history[-1][1] = last_move
+            else:
+                move_history[-1][0] = last_move
+
+            if self.__inactive_player__ == self.__player_1__:
+                output = "player1's "
+            else:
+                output = "player2's "
+            output += queen_history[-1][-1]
+            if last_move != last_attempt:
+                output += ' attempted (%d,%d),' % last_attempt
+            output += ' moved to (%d,%d)' % last_move
+            self.output_history.append(output)
+            if print_moves:
+                print output
+                print self.print_board()
 
 
-def game_as_text(winner, move_history, queen_history, termination="", board=Board(1, 2)):
+def game_as_text(winner, output_history, termination="", board=Board(1, 2)):
     print(winner)
     # ans = io.StringIO()
     ans = StringIO.StringIO()
     k = 0
-
     for i, move1 in enumerate(move_history):
         p1_move = move1[0]
-        ans.write(queen_history[k][0] + "  player1 " + "%d." % i + " (%d,%d)\r\n" % p1_move)
+        if len(output_history) > i * 2:
+            ans.write(output_history[i * 2] + '\r\n')
+        else:
+            ans.write('player1 lost: ' + termination + '\r\n')
+            ans.write("Winner: " + str(winner) + "\r\n")
+            ans.write(board.__active_player__ == board.__player_1__)
+            return ans.getvalue()
         if p1_move != Board.NOT_MOVED:
             board.__apply_move_write__(p1_move, queen_history[k][0])
         ans.write(board.print_board())
 
         if len(move1) > 1:
             p2_move = move1[1]
-            ans.write(queen_history[k][1] + " player2 " + "%d. ..." % i + " (%d,%d)\r\n" % p2_move)
+            if len(output_history) > i * 2 + 1:
+                ans.write(output_history[i * 2 + 1] + '\r\n')
+            else:
+                ans.write('player2 lost: ' + termination + '\r\n')
+                ans.write("Winner: " + str(winner) + "\r\n")
+                return ans.getvalue()
             if p2_move != Board.NOT_MOVED:
                 board.__apply_move_write__(p2_move, queen_history[k][1])
             ans.write(board.print_board())
         k = k + 1
-    ans.write(termination + "\r\n")
-    ans.write("Winner: " + str(winner) + "\r\n")
-
-    return ans.getvalue()
 
 
 if __name__ == '__main__':
@@ -425,5 +470,6 @@ if __name__ == '__main__':
     from test_players import HumanPlayer
 
     board = Board(RandomPlayer(), HumanPlayer())
-    winner, move_history, queen_history, termination = board.play_isolation(time_limit=500013)
-    print (game_as_text(winner, move_history, queen_history, termination))
+    board_copy = board.copy()
+    winner, move_history, queen_history, termination = board.play_isolation(time_limit=5000, print_moves=True)
+    print game_as_text(winner, board.output_history, termination, board_copy)
