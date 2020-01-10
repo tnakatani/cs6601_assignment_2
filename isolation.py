cc2c41ad7bd2c0ec190d25a19cc7166fd21205b1
pmcaughan6 <pmcaughan6@gatech.edu>
2020-01-09 18:04:08 -0600

    Removed swap mechanics

--- isolation.py
+++ isolation.py
@@ -18,10 +18,8 @@ class Board:
     BLANK = " "
     BLOCKED = "X"
     LASER = "O"
-    NOT_MOVED = (-1, -1, False)
+    NOT_MOVED = (-1, -1)
 
-    # Flag True when swap occured last turn
-    SWAP_FLAG = False
 
     __player_1__ = None
     __player_2__ = None
@@ -114,31 +112,22 @@ class Board:
         '''
         Apply chosen move to a board state and check for game end
         Parameters:
-            queen_move: (int, int, bool), Desired move to apply. Takes the 
-            form of (row, column, does this move cause a swap or not).
+            queen_move: (int, int), Desired move to apply. Takes the 
+            form of (row, column).
         Returns:
             result: (bool, str), Game Over flag, winner 
         '''
         # print("Applying move:: ", queen_move)
-        row, col, swap = queen_move
+        row, col = queen_move
         my_pos = self.__last_queen_move__[self.__active_players_queen__]
         opponent_pos = self.__last_queen_move__[self.__inactive_players_queen__]
 
         queen_name = self.__queen_symbols__[self.__active_players_queen__]
         self.__clear_laser__()
-        if (swap):
-            # Apply swap move
-            self.SWAP_FLAG = True
-            opponent_new_pos = (my_pos[0], my_pos[1], True)
-            self.__last_queen_move__[self.__inactive_players_queen__] = opponent_new_pos
-            self.__board_state__[opponent_new_pos[0]][opponent_new_pos[1]] = \
-                self.__queen_symbols__[self.__inactive_players_queen__]
-
-        else:
-            self.SWAP_FLAG = False
-            if self.move_is_in_board(my_pos[0], my_pos[1]):
-                self.__board_state__[my_pos[0]][my_pos[1]] = Board.BLOCKED
-                self.__create_laser__(queen_move, my_pos)
+   
+        if self.move_is_in_board(my_pos[0], my_pos[1]):
+            self.__board_state__[my_pos[0]][my_pos[1]] = Board.BLOCKED
+            self.__create_laser__(queen_move, my_pos)
 
         # apply move of active player
         self.__last_queen_move__[self.__active_players_queen__] = queen_move
@@ -169,8 +158,8 @@ class Board:
             None
         """
 
-        curr_row, curr_col, _ = current_position
-        prev_row, prev_col, _ = previous_position
+        curr_row, curr_col  = current_position
+        prev_row, prev_col  = previous_position
         vertical_iterator = 1
         horizontal_iterator = 1
 
@@ -231,15 +220,14 @@ class Board:
         b.__active_players_queen__ = self.__active_players_queen__
         b.__inactive_players_queen__ = self.__inactive_players_queen__
         b.__board_state__ = self.get_state()
-        b.SWAP_FLAG = self.SWAP_FLAG
         return b
 
     def forecast_move(self, queen_move):
         """
         See what board state would result from making a particular move without changing the board state itself.
         Parameters:
-            queen_move: (int, int, bool), Desired move to forecast. Takes the form of
-            (row, column, does this move cause a swap or not).
+            queen_move: (int, int), Desired move to forecast. Takes the form of
+            (row, column).
 
         Returns:
             (Board, bool, str): Resultant board from move, flag for game-over, winner (if game is over)
@@ -358,8 +346,8 @@ class Board:
         Parameters:
             None
         Returns:
-           [(int, int, bool)]: List of all legal moves. Each move takes the form of
-            (row, column, does this move cause a swap or not).
+           [(int, int)]: List of all legal moves. Each move takes the form of
+            (row, column).
         """
         q_move = self.__last_queen_move__[
             self.__inactive_players_queen__]
@@ -372,11 +360,12 @@ class Board:
         Parameters:
             None
         Returns:
-           [(int, int, bool)]: List of all legal moves. Each move takes the form of
-            (row, column, does this move cause a swap or not).
+           [(int, int)]: List of all legal moves. Each move takes the form of
+            (row, column).
         """
         q_move = self.__last_queen_move__[
             self.__active_players_queen__]
+
 
         return self.__get_moves__(q_move)
 
@@ -387,8 +376,8 @@ class Board:
             my_player (Player), Player to get moves for
             If calling from within a player class, my_player = self can be passed.
         returns
-            [(int, int, bool)]: List of all legal moves. Each move takes the form of
-            (row, column, does this move cause a swap or not).
+            [(int, int)]: List of all legal moves. Each move takes the form of
+            (row, column).
 
         """
         if (my_player == self.__player_1__ and self.__active_player__ == self.__player_1__):
@@ -410,8 +399,8 @@ class Board:
             my_player (Player), The player facing the opponent in question
             If calling from within a player class, my_player = self can be passed.
         returns
-            [(int, int, bool)]: List of all opponent's moves. Each move takes the form of
-            (row, column, does this move cause a swap or not).
+            [(int, int)]: List of all opponent's moves. Each move takes the form of
+            (row, column).
 
         """
         if (my_player == self.__player_1__ and self.__active_player__ == self.__player_1__):
@@ -430,17 +419,17 @@ class Board:
         Get all legal moves of a player on current board state as a list of possible moves. Not meant to be directly called, 
         use get_active_moves or get_inactive_moves instead.
         Parameters:
-            move: (int, int, bool), Last move made by player in question (where they currently are). 
-            Takes the form of (row, column, does this move cause a swap or not).
+            move: (int, int), Last move made by player in question (where they currently are). 
+            Takes the form of (row, column).
         Returns:
-           [(int, int, bool)]: List of all legal moves. Each move takes the form of
-            (row, column, does this move cause a swap or not).
+           [(int, int)]: List of all legal moves. Each move takes the form of
+            (row, column).
         """
 
         if move == self.NOT_MOVED:
             return self.get_first_moves()
 
-        r, c, _ = move
+        r, c = move
 
         directions = [(-1, -1), (-1, 0), (-1, 1),
                       (0, -1), (0, 1),
@@ -452,17 +441,9 @@ class Board:
             for dist in range(1, max(self.height, self.width)):
                 row = direction[0] * dist + r
                 col = direction[1] * dist + c
-                if self.move_is_in_board(row, col) and self.is_spot_open(row, col) and (row, col, False) not in moves:
-                    moves.append((row, col, False))
+                if self.move_is_in_board(row, col) and self.is_spot_open(row, col) and (row, col) not in moves:
+                    moves.append((row, col))
 
-                elif self.move_is_in_board(row, col) and self.is_spot_queen(row, col):
-                    for distance in range(1, dist + 1):
-                        if self.does_move_allow_swap(row, col, direction, distance) and not self.SWAP_FLAG and (
-                                row, col, True) not in moves:
-                            moves.append((row, col, True))
-                        else:
-                            break
-                    break
                 else:
                     break
 
@@ -474,10 +455,10 @@ class Board:
         Parameters:
             None
         Returns:
-           [(int, int, bool)]: List of all legal moves. Each move takes the form of
-            (row, column, does this move cause a swap or not).
+           [(int, int)]: List of all legal moves. Each move takes the form of
+            (row, column).
         """
-        return [(i, j, 0) for i in range(0, self.height)
+        return [(i, j) for i in range(0, self.height)
                 for j in range(0, self.width) if self.__board_state__[i][j] == Board.BLANK]
 
     def move_is_in_board(self, row, col):
@@ -515,43 +496,6 @@ class Board:
         q2 = self.__queen_symbols__[self.__inactive_players_queen__]
         return self.__board_state__[row][col] == q1 or self.__board_state__[row][col] == q2
 
-    def does_move_allow_swap(self, row, col, direction, distance):
-
-        """
-        Sanity check for checking if a move is a valid swap move
-        Parameters:
-            row: int, Row position of move in question
-            col: int, Column position of move in question
-            direction: (int, int), Directional unit vector of incoming queen
-            distance: int, Distance between [row,col] and incoming queen
-        Returns:
-            bool: Whether the [row,col] move is a valid swap move
-        """
-
-        if not self.is_spot_queen(row, col):
-            return False
-
-        # All spots up to the other queen to must be available to move to and be on the board
-        for dist in range(1, distance - 1):
-            row_target = row + direction[0] * dist
-            col_target = col + direction[1] * dist
-            if not self.move_is_in_board(row_target, col_target) or not self.is_spot_open(row_target, col_target):
-                return False
-
-        '''
-        row_target = row + direction[0] * distance
-        col_target = col + direction[1] * distance
-
-        if (row_target > self.height) or (row_target < -1) or \
-                (col_target > self.width) or (col_target < -1):
-            return False
-
-        if not self.move_is_in_board(row_target, col_target) or self.is_spot_open(row_target, col_target):
-            # and IF the square they'd be pushed TO is either off the board or an open space on the board
-            return True
-        '''
-
-        return True
 
     def space_is_open(self, row, col):
         """
@@ -571,14 +515,14 @@ class Board:
         """
         Function for printing board state & indicating possible moves for active player.
         Parameters:
-            legal_moves: [(int, int, bool)], List of legal moves to indicate when printing board spaces. 
-            Each move takes the form of (row, column, does this move cause a swap or not).
+            legal_moves: [(int, int)], List of legal moves to indicate when printing board spaces. 
+            Each move takes the form of (row, column).
         Returns:
             Str: Visual interpretation of board state & possible moves for active player
         """
 
-        p1_r, p1_c, swap = self.__last_queen_move__[self.__queen_1__]
-        p2_r, p2_c, swap = self.__last_queen_move__[self.__queen_2__]
+        p1_r, p1_c = self.__last_queen_move__[self.__queen_1__]
+        p2_r, p2_c = self.__last_queen_move__[self.__queen_2__]
         b = self.__board_state__
 
         out = '  |'
@@ -593,7 +537,7 @@ class Board:
                     out += self.__queen_symbols__[self.__queen_1__]
                 elif (i, j) == (p2_r, p2_c):
                     out += self.__queen_symbols__[self.__queen_2__]
-                elif (i, j, True) in legal_moves or (i, j, False) in legal_moves:
+                elif (i, j) in legal_moves or (i, j) in legal_moves:
                     out += 'o '
                 elif b[i][j] == Board.BLANK:
                     out += '  '
@@ -614,8 +558,8 @@ class Board:
             time_limit: int, time limit in milliseconds that each player has before they time out.
             print_moves: bool, Should the method print details of the game in real time
         Returns:
-            (str, [(int, int, bool)], str): Queen of Winner, Move history, Reason for game over.
-            Each move in move history takes the form of (row, column, does this move cause a swap or not).
+            (str, [(int, int)], str): Queen of Winner, Move history, Reason for game over.
+            Each move in move history takes the form of (row, column).
         """
         move_history = []
 
@@ -636,11 +580,6 @@ class Board:
 
             if print_moves:
                 print("\n", self.__active_players_queen__, " Turn")
-
-            if (self.SWAP_FLAG):
-                game_copy.SWAP_FLAG = True
-            else:
-                game_copy.SWAP_FLAG = False
 
             curr_move = self.__active_player__.move(
                 game_copy, time_left)  # queen added in return
@@ -681,8 +620,8 @@ class Board:
         Equivalent to __apply_move__, meant specifically for applying move history to a board 
         for analyzing an already played game.
         Parameters: 
-            move_queen: (int, int, bool), Move to apply to board. Takes
-            the form of (row, column, does this move cause a swap or not).
+            move_queen: (int, int), Move to apply to board. Takes
+            the form of (row, column).
         Returns:
             None
         """
@@ -690,33 +629,23 @@ class Board:
         if move_queen[0] is None or move_queen[1] is None:
             return
 
-        row, col, swap_flag = move_queen
+        row, col = move_queen
         my_pos = self.__last_queen_move__[self.__active_players_queen__]
         opponent_pos = self.__last_queen_move__[self.__inactive_players_queen__]
 
-        if (move_queen[2]):
-            # Apply swap move
-            opponent_new_pos = (my_pos[0], my_pos[1], True)
-            self.__last_queen_move__[self.__inactive_players_queen__] = opponent_new_pos
-            self.__last_queen_move__[self.__active_players_queen__] = (row, col, True)
-            self.__board_state__[opponent_new_pos[0]][opponent_new_pos[1]] = \
-                self.__queen_symbols__[self.__inactive_players_queen__]
-            self.__board_state__[row][col] = \
-                self.__queen_symbols__[self.__active_players_queen__]
-        else:
-            self.__last_queen_move__[self.__active_players_queen__] = move_queen
-            self.__board_state__[row][col] = \
-                self.__queen_symbols__[self.__active_players_queen__]
+        self.__last_queen_move__[self.__active_players_queen__] = move_queen
+        self.__board_state__[row][col] = \
+            self.__queen_symbols__[self.__active_players_queen__]
 
         if self.move_is_in_board(my_pos[0], my_pos[1]):
             self.__board_state__[my_pos[0]][my_pos[1]] = Board.BLOCKED
 
-        # swap the players
+        # Rotate the active player
         tmp = self.__active_player__
         self.__active_player__ = self.__inactive_player__
         self.__inactive_player__ = tmp
 
-        # swapping the queens
+        # Rotate the active queen
         tmp = self.__active_players_queen__
         self.__active_players_queen__ = self.__inactive_players_queen__
         self.__inactive_players_queen__ = tmp
@@ -744,8 +673,8 @@ def game_as_text(winner, move_history, termination="", board=Board(1, 2)):
     """
     Function to play out a move history on a new board. Used for analyzing an interesting move history 
     Parameters: 
-        move_history: [(int, int, bool)], History of all moves in order of game in question. 
-        Each move takes the form of (row, column, does this move cause a swap or not).
+        move_history: [(int, int)], History of all moves in order of game in question. 
+        Each move takes the form of (row, column).
         termination: str, Reason for game over of game in question. Obtained from play_isolation
         board: Board, board that game in question was played on. Used to initialize board copy
     Returns:
@@ -768,28 +697,13 @@ def game_as_text(winner, move_history, termination="", board=Board(1, 2)):
             board.__apply_move_write__(move[0])
             ans.write("\n\n" + board.__queen_1__ + " moves to (" + str(move[0][0]) + "," + str(move[0][1]) + ")\r\n")
 
-            if len(move) > 1 and move[0][2] is True:
-                my_x, my_y = last_move[0][0], last_move[0][1]
-                enemy_x, enemy_y = move[0][0], move[0][1]
-
-                new_enemy_x, new_enemy_y = my_x, my_y
-
-                ans.write(
-                    "\n\n" + board.__queen_2__ + " swapped to (" + str(new_enemy_x) + "," + str(new_enemy_y) + ")\r\n")
-
+            
         if len(move) > 1 and move[1] != Board.NOT_MOVED and move[0] is not None:
             ans.write(board.print_board())
             board.__apply_move_write__(move[1])
             ans.write("\n\n" + board.__queen_2__ + " moves to (" + str(move[1][0]) + "," + str(move[1][1]) + ")\r\n")
 
-            if move[1] is not None and move[1][2] is True:
-                my_x, my_y = last_move[1][0], last_move[1][1]
-                new_enemy_x, new_enemy_y = move[1][0], move[1][1]
-                new_enemy_x, new_enemy_y = my_x, my_y
-
-                ans.write(
-                    "\n\n" + board.__queen_1__ + " swapped to (" + str(new_enemy_x) + "," + str(new_enemy_y) + ")\r\n")
-
+            
         last_move = move
 
     ans.write("\n" + str(winner) + " has won. Reason: " + str(termination))
